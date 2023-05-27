46c27921abb9ee7958f6f52094fd98a0fb969496
nhatnv0378 <nguyenvietnhat.03@gmail.com>
2023-05-27 10:00:44 +0700

    Init evaluation.py

---
+++ HOG_SVM/evaluation.py
@@ -0,0 +1,17 @@
+import numpy as np
+import json
+import glob
+import os
+
+
+eval_images = './eval/images'
+eval_annotations = './eval/annotations'
+
+
+def main():
+    for img in glob.glob(os.path.join(eval_images), "*"):
+       pass 
+
+
+if __name__ == '__main__':
+    main()
